a45b4a557cffba7b613bc864bbe840e31890545d
OwenxOwen <oreynold@ashland.edu>
2025-03-12 17:08:24 -0400

    Finished Shell

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import './Header.css';
 
+import profilepic from '../../assets/IMG_0686.png';
+
 export default function Header() {
     return (
         <div className="headerContainer">
+            <div className="navSection">
+                <h4>About</h4>
+                <h4>Work</h4>
+                <h4>Life</h4>
+            </div>
             <div className="photoContainer">
-                <img src="" alt="" />
+                <img src={profilepic} alt="" />
             </div>
             <div className="headerTextContainer">
                 <h2>Owen Reynolds</h2>
                 <h4>Aspiring Software Engineer</h4>
             </div>
+            <div className="mediaIcons">
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />  
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-plain.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/twitter/twitter-original.svg" />
+            </div>
+            
         </div>
     )
 }
--- frontend/src/sections/firstSection/FirstSection.jsx
+++ frontend/src/sections/firstSection/FirstSection.jsx
@@ -3,9 +3,15 @@ import './FirstSection.css';
 export default function FirstSection() {
     return (
         <>
-        <div className="holder">
-            <div className="testing"></div>
-            <div className="testingagain"></div>
+        <div className="firstSectionContainer">
+            <div className="firstSectionContent">
+                <h2>Hello World, I'm Owen</h2>
+                <p>I am currently a junior at Ashland University pursuing a B.S. in Computer Science. 
+                    I am passionate about software engineering, web development, and AI. 
+                    I also completed my A.A. in Business Administration prior to attending Ashland University. 
+                    In my free time, I enjoy exercising, spending time outdoors, and playing basketball. 
+                </p>
+            </div>
         </div>
         
         </>
